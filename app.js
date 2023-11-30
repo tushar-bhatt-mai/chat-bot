4955cbee57ae32fd1725560ffb560bf987024e53
Tushar Bhatt <Tushar.Bhatt@P2E.COM>
2023-11-30 10:29:05 +0530

    refresh token handler

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const chatBox = document.getElementById("chat-box");
 const userInput = document.getElementById("user-input");
 const baseUrl = "https://dev-maibot-predictionapi.p2eppl.com/";
 let chatBotResult = {};
-
-const loggedInUserData = (data)=>{
-  const roundedValue = data?.display_name.split(" ").map((word) => word[0]).join("")
+const params = parseQueryString(window.location.href);
+let accessToken = params?.token;
+let refreshToken = params?.refreshToken;
+const loggedInUserData = (data) => {
+  const roundedValue = data?.display_name.split(" ").map((word) => word[0]).join("");
   const roundedData = document.getElementById("roundedName");
-  roundedData.innerHTML = `${roundedValue}`
+  roundedData.innerHTML = `${roundedValue}`;
 }
-
 function initialMessage() {
   loggedInUserData(chatBotResult)
   const chatContainer = document.getElementById("chat-container");
-  chatContainer.style.backgroundColor = chatBotResult?.background_colour
+  chatContainer.style.backgroundColor = chatBotResult?.background_colour;
   const displayName = document.getElementById("display-name");
-  displayName.innerHTML = `${chatBotResult?.display_name ? chatBotResult?.display_name : ``}`
+  displayName.innerHTML = `${chatBotResult?.display_name ? chatBotResult?.display_name : ``}`;
   const initialValue = document.getElementById("initialValue");
   initialValue.innerHTML = `<span class='bot-message'>${
     chatBotResult?.initial_message
@@ -26,11 +27,9 @@ function initialMessage() {
   initialValue.style.fontFamily = chatBotResult?.font_style || "Arial, sans-serif";
   displaySuggestedMessages(chatBotResult?.suggested_messages || []);
 }
-
 function displaySuggestedMessages(suggestedMessages) {
   const suggestedMessagesContainer = document.getElementById("suggested-messages");
   suggestedMessagesContainer.innerHTML = "";
-
   suggestedMessages.forEach((message) => {
     const suggestionButton = document.createElement("button");
     suggestionButton.textContent = message;
@@ -41,64 +40,59 @@ function displaySuggestedMessages(suggestedMessages) {
     suggestedMessagesContainer.appendChild(suggestionButton);
   });
 }
-
 function fillInputAndSendMessage(message) {
   const userInput = document.getElementById("user-input");
   userInput.value = message;
   sendMessage();
-  // By default we hide the suggestion second time
+  // By default, hide the suggestion second time
   const suggestedMessagesContainer = document.getElementById("suggested-messages");
   suggestedMessagesContainer.style.display = "none";
 }
-
 function parseQueryString(url) {
   const queryString = url.split("?")[1];
   const queryParams = new URLSearchParams(queryString);
   return Object.fromEntries(queryParams.entries());
 }
-
-const params = parseQueryString(window.location.href);
-
 function sendMessage() {
   const message = userInput.value.trim();
   if (!message) return;
   displayMessage(message, "user");
   userInput.value = "";
   const myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${params.token}`);
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
   const formdata = new FormData();
   formdata.append("username", params.username);
   formdata.append("modelname", params.modelname);
   formdata.append("question", message);
   formdata.append("source_language_code", "en");
-
   const requestOptions = {
     method: "POST",
     headers: myHeaders,
     body: formdata,
     redirect: "follow",
   };
-
   fetch(baseUrl, requestOptions)
     .then((response) => response.text())
     .then((result) => {
-      var parsedResult ={};
-      if(typeof result !== "string"){
-        parsedResult = JSON.parse(result || "{}");
-      }
-      if (parsedResult.StatusCode === "401") {
-        displayMessage("Something went Wrong", "bot");
+      var parsedResult = JSON.parse(result || "{}")
+      console.log("parse result", );
+      if (parsedResult.StatusCode==401) {
+        // Refresh the access token
+        fetchRefreshtoken();
+        displayMessage("Something went Wrong, Please try again", "bot");
+        // Retry the message sending with the new token
+        // sendMessage();
       } else {
+        // Process the result as usual
         const str = JSON.stringify(result);
         displayMessage(JSON.parse(result)?.message, "bot");
       }
     })
     .catch((error) => console.error("error", error));
 }
-
 function getChatbotDetails() {
   const myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${params.token}`);
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
   const formdata = new FormData();
   formdata.append("username", params.username);
   formdata.append("chatbot_name", params.chatbotname);
@@ -108,11 +102,10 @@ function getChatbotDetails() {
     body: formdata,
     redirect: "follow",
   };
-
   fetch("https://dev-maibot-chatbot-detailapi.p2eppl.com/get_chatbot_customization", requestOptions)
     .then((response) => response.text())
     .then((result) => {
-      const parsedResult = JSON.parse(result);  
+      const parsedResult = JSON.parse(result);
       if (parsedResult.statusCode === 200) {
         chatBotResult = parsedResult.message;
         initialMessage();
@@ -120,7 +113,6 @@ function getChatbotDetails() {
     })
     .catch((error) => console.error("error", error));
 }
-
 function displayMessage(message, sender) {
   const messageElement = document.createElement("div");
   messageElement.classList.add(sender === "user" ? "user-messageDev" : "bot-messageDev");
@@ -130,6 +122,24 @@ function displayMessage(message, sender) {
   messageElement.appendChild(textSpan);
   chatBox.appendChild(messageElement);
 }
-
-
 getChatbotDetails();
+const fetchRefreshtoken = async () => {
+  try {
+    const response = await fetch(`https://dev-auth2api.p2eppl.com/auth/refresh`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "refresh_token": refreshToken,
+      }),
+    });
+    const responseData = await response.json();
+    if (responseData?.result) {
+      accessToken = responseData?.result?.access_token;
+      sendMessage();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
