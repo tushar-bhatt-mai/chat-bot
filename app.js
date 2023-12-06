9b3e6f069ec72d86384061c036dd3ab3c8f3bcca
nitesh-mai <nitesh-mai>
2023-12-06 15:15:45 +0530

    code refactor

--- app.js
+++ app.js
@@ -1,193 +1,353 @@
+// DOM elements
 const chatBox = document.getElementById("chat-box");
-// const loader = document.createElement("div");
-// chatBox.appendChild(loader);
-// loader.classList.add("loader")
 const userInput = document.getElementById("user-input");
-const baseUrl = "https://dev-maibot-predictionapi.p2eppl.com/";
+
+// API endpoints
+const baseUrlPrediction = "https://dev-maibot-predictionapi.p2eppl.com/";
+const baseUrlCostomization =
+  "https://dev-maibot-chatbot-detailapi.p2eppl.com/get_chatbot_customization";
+
+// Global variables
 let chatBotResult = {};
 const params = parseQueryString(window.location.href);
-let accessToken = params?.token;
+let accessToken = getLocalStorageItem("accessTokenIframe") || params?.token;
 let refreshToken = params?.refreshToken;
 var loading = false;
+
+// Function to display logged-in user data
 const loggedInUserData = (data) => {
-  const roundedValue = data?.display_name
+  const roundedValue = getRoundedValue(data);
+  const roundedData = document.getElementById("roundedName");
+
+  if (data?.profile_picture_url !== "NA") {
+    displayProfilePicture(roundedData, data?.profile_picture_url);
+  } else if (data?.display_name) {
+    displayRoundedName(roundedData, roundedValue);
+  } else {
+    roundedData.style.backgroundColor = "transparent";
+  }
+};
+
+// Helper function to get rounded value from display name
+function getRoundedValue(data) {
+  return data?.display_name
     .split(" ")
     .map((word) => word[0])
     .join("");
-  const roundedData = document.getElementById("roundedName");
-  var imgElement = document.createElement("img");
-  imgElement.src = `${data?.profile_picture_url}`;
+}
+
+// Helper function to display profile picture
+function displayProfilePicture(container, imageUrl) {
+  const imgElement = createImageElement(imageUrl);
+  container.appendChild(imgElement);
+}
+
+// Helper function to create image element
+function createImageElement(imageUrl) {
+  const imgElement = document.createElement("img");
+  imgElement.src = imageUrl;
   imgElement.classList.add("imgStyle");
-  data?.profile_picture_url !== "NA"
-    ? roundedData.appendChild(imgElement)
-    : data?.display_name
-    ? ((roundedData.style.backgroundColor = "#6D1874"),
-      (roundedData.innerHTML = `${roundedValue}`))
-    : (roundedData.style.backgroundColor = "transparen");
-  // roundedData.innerHTML = `${roundedValue}`;
-};
+  return imgElement;
+}
+
+// Helper function to display rounded name
+function displayRoundedName(container, value) {
+  container.style.backgroundColor = "#6D1874";
+  container.innerHTML = value;
+}
+
+// Function to initialize the chat interface
 function initialMessage() {
   loggedInUserData(chatBotResult);
+  setChatNavbarStyle();
+  setDisplayChatWith();
+  setDisplayName();
+  setUserInputStyle();
+  setInitialValue();
+  displaySuggestedMessages(
+    chatBotResult?.suggested_messages,
+    chatBotResult?.font_size || []
+  );
+  setChatbotStyle();
+}
+
+// Helper function to set chat navbar style
+function setChatNavbarStyle() {
   const chatNavbar = document.getElementById("chat-Navbar");
-  chatBotResult?.background_colour
-    ? (chatNavbar.style.backgroundColor = chatBotResult?.background_colour)
-    : chatNavbar.classList.add("bgColor");
+  const backgroundColor = chatBotResult?.background_colour || "bgColor";
+  chatNavbar.style.backgroundColor = backgroundColor;
+}
+
+// Helper function to set "Chat With" display
+function setDisplayChatWith() {
   const displayChatWith = document.getElementById("display-chat");
-  chatBotResult?.display_name
-    ? (displayChatWith.innerHTML = "Chat With")
-    : (displayChatWith.innerHTML = "");
-    displayChatWith.style.fontSize = `${chatBotResult?.font_size}px` || "12px"
+  displayChatWith.innerHTML = chatBotResult?.display_name ? "Chat With" : "";
+  displayChatWith.style.fontSize = `${chatBotResult?.font_size}px` || "12px";
+}
+
+// Helper function to set display name
+function setDisplayName() {
   const displayName = document.getElementById("display-name");
-  displayName.innerHTML = `${
-    chatBotResult?.display_name ? chatBotResult?.display_name : ``
-  }`;
-  displayName.style.fontSize = `${chatBotResult?.font_size}px` || "12px"
-  userInput.style.fontSize = `${chatBotResult?.font_size}px` || "12px"
+  displayName.innerHTML = chatBotResult?.display_name || "";
+  displayName.style.fontSize = `${chatBotResult?.font_size}px` || "12px";
+}
+
+// Helper function to set user input style
+function setUserInputStyle() {
+  userInput.style.fontSize = `${chatBotResult?.font_size}px` || "12px";
+}
+
+// Helper function to set initial value
+function setInitialValue() {
   const initialValue = document.getElementById("initialValue");
   initialValue.innerHTML = `<span class='bot-message'>${
-    chatBotResult?.initial_message
-      ? chatBotResult?.initial_message
-      : `Initial Message`
+    chatBotResult?.initial_message || "Initial Message"
   }</span>`;
-  displaySuggestedMessages(chatBotResult?.suggested_messages, chatBotResult?.font_size || []);
-
   initialValue.style.fontSize = `${chatBotResult?.font_size}px` || "12px";
-  document.getElementById('chatbot-style').style.fontFamily = chatBotResult?.font_style || "Arial, sans-serif";
 }
+
+// Helper function to set chatbot style
+function setChatbotStyle() {
+  const chatbotStyle = document.getElementById("chatbot-style");
+  chatbotStyle.style.fontFamily =
+    chatBotResult?.font_style || "Arial, sans-serif";
+}
+
+// Function to display suggested messages
 function displaySuggestedMessages(suggestedMessages, fontSize) {
-  const suggestedMessagesContainer = document.getElementById(
-    "suggested-messages"
-  );
-  suggestedMessagesContainer.innerHTML = "";
+  const suggestedMessagesContainer = getSuggestedMessagesContainer();
+  clearSuggestedMessages(suggestedMessagesContainer);
+
   suggestedMessages.forEach((message) => {
-    const suggestionButton = document.createElement("button");
-    suggestionButton.textContent = message;
-    suggestionButton.classList.add("suggestion-button");
-    suggestionButton.style.fontSize = `${fontSize}px` || "12px";
-    suggestionButton.onclick = function () {
-      fillInputAndSendMessage(message);
-    };
-    suggestedMessagesContainer.appendChild(suggestionButton);
+    const suggestionButton = createSuggestionButton(message, fontSize);
+    addClickEventToSuggestionButton(suggestionButton, message);
+    appendSuggestionButton(suggestedMessagesContainer, suggestionButton);
   });
 }
+
+// Helper function to get suggested messages container
+function getSuggestedMessagesContainer() {
+  return document.getElementById("suggested-messages");
+}
+
+// Helper function to clear suggested messages
+function clearSuggestedMessages(container) {
+  container.innerHTML = "";
+}
+
+// Helper function to create suggestion button
+function createSuggestionButton(message, fontSize) {
+  const suggestionButton = document.createElement("button");
+  suggestionButton.textContent = message;
+  suggestionButton.classList.add("suggestion-button");
+  suggestionButton.style.fontSize = `${fontSize}px` || "12px";
+  return suggestionButton;
+}
+
+// Helper function to add click event to suggestion button
+function addClickEventToSuggestionButton(button, message) {
+  button.onclick = function () {
+    fillInputAndSendMessage(message);
+  };
+}
+
+// Helper function to append suggestion button
+function appendSuggestionButton(container, button) {
+  container.appendChild(button);
+}
+
+// Helper function to hide suggested messages
+function hideSuggestionsMsg() {
+  document.getElementById("suggested-messages").style.display = "none";
+}
+
+// Helper function to fill input and send message
 function fillInputAndSendMessage(message) {
   const userInput = document.getElementById("user-input");
   userInput.value = message;
   sendMessage();
-  // By default, hide the suggestion second time
-  const suggestedMessagesContainer = document.getElementById(
-    "suggested-messages"
-  );
-  suggestedMessagesContainer.style.display = "none";
+  hideSuggestionsMsg();
 }
+
+// Helper function to parse query string from URL
 function parseQueryString(url) {
   const queryString = url.split("?")[1];
   const queryParams = new URLSearchParams(queryString);
   return Object.fromEntries(queryParams.entries());
 }
-function sendMessage() {
-  const message = userInput.value.trim();
-  if (!message) return;
+
+// Helper function to get user input message
+function getUserInputMessage() {
+  return userInput.value.trim();
+}
+
+// Helper function to display user message
+function displayUserMessage(message) {
   displayMessage(message, "user", false);
+}
+
+// Helper function to clear user input
+function clearUserInput() {
   userInput.value = "";
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${accessToken}`);
+}
+
+// Helper function to create headers for API requests
+function createHeaders() {
+  const headers = new Headers();
+  headers.append("Authorization", `Bearer ${accessToken}`);
+  return headers;
+}
+
+// Helper function to create form data for sending messages
+function createFormDataForSendMsg(message) {
   const formdata = new FormData();
-  formdata.append("username", params.username);
-  formdata.append("modelname", params.modelname);
+  formdata.append("username", params?.username);
+  formdata.append("modelname", params?.modelname);
   formdata.append("question", message);
   formdata.append("source_language_code", "en");
-  const requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: formdata,
-    redirect: "follow",
-  };
-  loading = true;
-  fetch(baseUrl, requestOptions)
-    .then((response) => response.text())
-    .then((result) => {
-      var parsedResult = JSON.parse(result || "{}");
-      console.log("parse result");
-      if (parsedResult.StatusCode == 401) {
-        fetchRefreshtoken();
-        displayMessage("Something went Wrong, Please try again", "bot", true);
-      } else {
-        const str = JSON.stringify(result);
-        displayMessage(JSON.parse(result)?.message, "bot", true);
-        loading = false;
-      }
-    })
-    .catch((error) => {
-      console.error("error", error);
-    })
-    .finally(() => {
-      loading = false;
-    });
+  return formdata;
 }
-function scrollToBottom() {
-  var chatWindow = document.getElementById("chat-box");
-  if (chatWindow) {
-    chatWindow.scrollTop = chatWindow.scrollHeight;
+
+// Function to send user message to the chatbot
+function sendMessage() {
+  const message = getUserInputMessage();
+  if (!message) return;
+  displayUserMessage(message);
+  clearUserInput();
+  hideSuggestionsMsg();
+  const formData = createFormDataForSendMsg(message);
+  const myHeaders = createHeaders();
+  const requestOptions = createRequestOptions(myHeaders, formData);
+  loading = true;
+  fetchMessage(baseUrlPrediction, requestOptions);
+}
+
+// Helper function to handle the result of the chatbot API
+function handleMessageResult(result) {
+  const parsedResult = JSON.parse(result || "{}");
+  if (parsedResult.StatusCode == 401) {
+    fetchRefreshtoken();
+    displayMessage("Something went Wrong, Please try again", "bot", true);
+  } else {
+    displayBotMessage(JSON.parse(result)?.message);
   }
 }
-function getChatbotDetails() {
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${accessToken}`);
+
+// Helper function to display bot message in the chat
+function displayBotMessage(message) {
+  displayMessage(message, "bot", true);
+}
+
+// Helper function to handle errors
+function handleError(error) {
+  console.error("error", error);
+}
+
+// Helper function to create form data for API requests
+function createFormData(params) {
   const formdata = new FormData();
   formdata.append("username", params.username);
   formdata.append("chatbot_name", params.chatbotname);
-  const requestOptions = {
+  return formdata;
+}
+
+// Helper function to create options for API requests
+function createRequestOptions(headers, body) {
+  return {
     method: "POST",
-    headers: myHeaders,
-    body: formdata,
+    headers: headers,
+    body: body,
     redirect: "follow",
   };
-  fetch(
-    "https://dev-maibot-chatbot-detailapi.p2eppl.com/get_chatbot_customization",
-    requestOptions
-  )
-    .then((response) => response.text())
-    .then((result) => {
-      const parsedResult = JSON.parse(result);
-      if (parsedResult.statusCode === 200) {
-        chatBotResult = parsedResult.message;
-        initialMessage();
-      }
-    })
-    .catch((error) => console.error("error", error));
 }
-function displayMessage(message, sender, flag) {
-  const messageElement = document.createElement("div");
-  messageElement.classList.add(
-    sender === "user" ? "user-messageDev" : "bot-messageDev"
-  );
-  const textSpan = document.createElement("span");
- 
-  textSpan.classList.add(sender === "user" ? "user-message" : "bot-message");
-  textSpan.style.fontSize = `${chatBotResult?.font_size}px`
 
-  if (loading && flag && sender === "bot") {
-    const loaderDiv = document.createElement("div");
-    loaderDiv.className = "loader";
-    for (let i = 0; i < 3; i++) {
-      const spanElement = document.createElement("span");
-      loaderDiv.appendChild(spanElement);
-    }
-    textSpan.appendChild(loaderDiv);
-    setTimeout(() => {
-      loaderDiv.style.display = "none";
-      textSpan.textContent = message;
-    }, 1000);
-  } else {
-    textSpan.textContent = message;
+// Function to get chatbot details on page load
+function getChatbotDetails() {
+  const myHeaders = createHeaders();
+  const formdata = createFormData(params);
+  fetchChatbotDetails(createRequestOptions(myHeaders, formdata));
+}
+
+// Helper function to handle the result of fetching chatbot details
+function handleChatbotDetailsResult(result) {
+  const parsedResult = JSON.parse(result);
+  if (parsedResult.statusCode === 200) {
+    chatBotResult = parsedResult.message;
+    initialMessage();
   }
+}
+
+// Function to display a message in the chat
+function displayMessage(message, sender, flag) {
+  const messageElement = createMessageElement(sender);
+  const textSpan = createTextSpan(message, sender, flag);
   loading = false;
   messageElement.appendChild(textSpan);
   chatBox.appendChild(messageElement);
   scrollToBottom();
 }
-getChatbotDetails();
+
+// Helper function to create a message element
+function createMessageElement(sender) {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add(
+    sender === "user" ? "user-messageDev" : "bot-messageDev"
+  );
+  return messageElement;
+}
+
+// Helper function to create a text span element
+function createTextSpan(message, sender, flag) {
+  const textSpan = document.createElement("span");
+  textSpan.classList.add(sender === "user" ? "user-message" : "bot-message");
+  textSpan.style.fontSize = `${chatBotResult?.font_size}px`;
+
+  if (loading && flag && sender === "bot") {
+    createLoader(textSpan, message);
+  } else {
+    textSpan.textContent = message;
+  }
+
+  return textSpan;
+}
+
+// Helper function to create a loader element
+function createLoader(parentElement, message) {
+  const loaderDiv = document.createElement("div");
+  loaderDiv.className = "loader";
+
+  for (let i = 0; i < 3; i++) {
+    const spanElement = document.createElement("span");
+    loaderDiv.appendChild(spanElement);
+  }
+
+  parentElement.appendChild(loaderDiv);
+
+  setTimeout(() => {
+    loaderDiv.style.display = "none";
+    parentElement.textContent = message;
+  }, 1000);
+}
+
+// Helper function to scroll to the bottom of the chat window
+function scrollToBottom() {
+  let chatWindow = document.getElementById("chat-box");
+  if (chatWindow) {
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+  }
+}
+
+// Helper function to get an item from local storage
+function getLocalStorageItem(key) {
+  return localStorage.getItem(key);
+}
+
+// Helper function to set an item in local storage
+function setLocalStorageItem(key, value) {
+  return localStorage.setItem(key, value);
+}
+
+// Function to refresh the access token using the refresh token
 const fetchRefreshtoken = async () => {
   try {
     const response = await fetch(
@@ -205,9 +365,32 @@ const fetchRefreshtoken = async () => {
     const responseData = await response.json();
     if (responseData?.result) {
       accessToken = responseData?.result?.access_token;
+      setLocalStorageItem("accessTokenIframe", responseData?.result?.access_token);
       sendMessage();
     }
   } catch (error) {
     console.error(error);
   }
 };
+
+// Function to fetch message from the chatbot API
+function fetchMessage(url, requestOptions) {
+  fetch(url, requestOptions)
+    .then((response) => response.text())
+    .then(handleMessageResult)
+    .catch(handleError)
+    .finally(() => {
+      loading = false;
+    });
+}
+
+// Function to fetch chatbot details from the API
+function fetchChatbotDetails(requestOptions) {
+  fetch(baseUrlCostomization, requestOptions)
+    .then((response) => response.text())
+    .then(handleChatbotDetailsResult)
+    .catch((error) => console.error("error", error));
+}
+
+// Fetch chatbot details on page load
+getChatbotDetails();
